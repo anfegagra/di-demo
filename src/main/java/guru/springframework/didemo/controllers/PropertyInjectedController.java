0238753b00f5ec04366acc8ce95a79df8957928a
Andrés Felipe García Granados <andres.granados@ceiba.com.co>
2019-09-17 21:36:26 -0500

    inyección de dependencias por propiedad

---
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    @Autowired
+    public GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayHello();
+    }
+}
