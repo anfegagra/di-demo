2492ec1f4e9a446f4ca6a2154fc6f15782645706
Andrés Felipe García Granados <andres.granados@ceiba.com.co>
2019-09-17 21:37:33 -0500

    inyección de dependencias por constructor

---
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,18 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayHello();
+    }
+}
