a46e76a9d20c2d366e6f4324b5ec0e9238da3443
Andrés Felipe García Granados <andres.granados@ceiba.com.co>
2019-09-17 20:16:47 -0500

    empezando ejemplo de inyección de dependencias

---
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -0,0 +1,18 @@
+package guru.springframework.didemo;
+
+import guru.springframework.didemo.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+        MyController controller = (MyController) ctx.getBean("myController");
+        controller.sayHello();
+    }
+
+}
---
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package guru.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello!");
+        return "goku";
+    }
+}
