f284b7d070e49151e081fc5750856e80ba42755d
anfegagra <andres.granados@ceiba.com.co>
2020-05-15 21:13:32 -0500

    se refactoriza para tener varios archivos de propiedades dentro de la anotación @PropertySources({})

--- src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+	@PropertySource("classpath:datasource.properties"),
+	@PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
 	@Value("${guru.username}")
